7e7b74f1a778c354750a0e7115b62349c6ebe79b
Vimalraj <technievimal@gmail.com>
2016-09-08 14:13:53 +0530

    Update for_guvi_pos_neg_zero.c

--- for_guvi_pos_neg_zero.c
+++ for_guvi_pos_neg_zero.c
@@ -1,8 +1,14 @@
-n=int(input('Enter the number '))
-if(n!=0):
-    if(n>0):
-        print('Positive')
-    else:
-        print('Negative')
-else:
-    print('Zero')
+#include <stdio.h>
+int main()
+{
+    double number;
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+    if (number < 0.0)
+        printf("You entered a negative number.");
+    else if ( number > 0.0)
+        printf("You entered a positive number.");
+    else
+        printf("You entered 0.");
+    return 0;
+}
